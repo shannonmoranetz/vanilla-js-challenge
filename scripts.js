eb365710c6357f0aced3442707c35eb9b2e53103
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-02-07 12:50:45 -0700

    Show guess results on dom

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 var minRange = 0;
 var maxRange = 100;
+var answer = 50;
 
 document.querySelector('.guess-submit').addEventListener('click', function() {
   getNameGuess();
@@ -10,8 +11,38 @@ function getNameGuess() {
   var playerTwoGuess = document.querySelector('.p2-guess-input').value;
   var playerOneName = document.querySelector('.p1-name-input').value;
   var playerTwoName = document.querySelector('.p2-name-input').value;
-  console.log(playerOneGuess, playerTwoGuess, playerOneName, playerTwoName);
+  checkPlayerOneGuess(playerOneGuess, playerOneName);
+  checkPlayerTwoGuess(playerTwoGuess, playerTwoName);
 }
 
-// function checkGuess() {
-// }
+function checkPlayerOneGuess(playerOneGuess, playerOneName) {
+  var playerOneResult;
+  if (playerOneGuess < answer) {
+    playerOneResult = 'thats too low';
+  } else if (playerOneGuess > answer) {
+    playerOneResult = 'thats too high';
+  } else {
+    playerOneResult = 'correct';
+  }
+  updatePlayerOneScoreDom(playerOneResult);
+}
+
+function checkPlayerTwoGuess(playerTwoGuess, playerTwoName) {
+  var playerTwoResult;
+  if (playerTwoGuess < answer) {
+    playerTwoResult = 'thats too low';
+  } else if (playerTwoGuess > answer) {
+    playerTwoResult = 'thats too high';
+  } else {
+    playerTwoResult = 'correct';
+  }
+  updatePlayerTwoScoreDom(playerTwoResult);
+}
+
+function updatePlayerOneScoreDom(result) {
+  document.querySelector('.p1-current-guess').innerHTML = result;
+}
+
+function updatePlayerTwoScoreDom(result) {
+  document.querySelector('.p2-current-guess').innerHTML = result;
+}
